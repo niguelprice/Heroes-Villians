69539b42c0a53188625b3531cfea7c4d7734ae43
Niguel Price <niguelprice@yahoo.com>
2022-09-24 15:57:45 -0400

    updated views file

--- supers/views.py
+++ supers/views.py
@@ -1,10 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def Supers_list(request):
-    supers = Super.objects.all()
-    serializer = SuperSerializer(supers, many=True)
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+
+
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
