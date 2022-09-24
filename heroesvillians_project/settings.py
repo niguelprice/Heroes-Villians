9ea0aee5250609b7a30e1d48dd1be03339ed26e3
Niguel Price <niguelprice@yahoo.com>
2022-09-24 03:55:19 -0400

    updated project urls + created view functions

--- heroesvillians_project/settings.py
+++ heroesvillians_project/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'super_types',
-    'supers'
-
+    'supers',
+    'super_types'
 ]
 
 MIDDLEWARE = [
